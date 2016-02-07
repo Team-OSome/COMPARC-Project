bb55f72c49958de6071d79af02029aa060818c85
Matthew Deogracais <nolan0597@yahoo.com>
2016-02-07 21:42:50 +0800

    Arranging stuff

--- Test folders/cosx.cpp
+++ Test folders/cosx.cpp
@@ -1,41 +1,36 @@
 #include <stdio.h>
 #include <math.h>
+
 /* Calculates cos(x) by using a Taylor approximation:
    cos(x) = x^0/(0!) - x^2/(2!) + x^4/(4!) - x^6/(6!) + x^8/(8!) - ... */
 
 int main(void)
 {    
-    int   k,         // dummy variable k
-    						epsilon,			// number of iterations
-    						n = 0;					// count of iterations for testing purposes
+    int   k = 0,         // dummy variable k
+    						epsilon,							// number of iterations
+    						n = 0;									// count of iterations for testing purposes
 
-    float x,         // parameter of cos(x), in radians
-          //epsilon,   // precision of cos(x) (cos = sum ± epsilon)
-          sum,       // sum of the terms of the polynomial series
-          term;      // variable that stores each term of the summation
+    float x,         				// parameter of cos(x), in radians
+          sum = 1,       // sum of the terms of the polynomial series
+          term = 1;      // variable that stores each term of the summation
 
-    printf("Enter x: ");
+    printf("Enter x in radians: ");
 				scanf("%f", &x);
 				printf("Enter epsilon (number of iterations): ");
-				//scanf("%f", &epsilon);
 				scanf("%d", &epsilon);
 
-    sum = term = 1, k = 0;
-
-    //while (fabs(term) >= epsilon)
-    while (epsilon > 0)
-    // while absolute of term is smaller than epsilon
+    while (epsilon > 0)   // while the summation is not yet done
     {
         k += 2;
         term *= -(x*x)/(k*(k-1));
         sum += term;
-        printf("K = %d \nx = %d\nsum = %f\nterm = %f\n", k*(k-1), n, sum, term);
-        n++;
         epsilon--;
+        //printf("K = %d \nx = %d\nsum = %f\nterm = %f\n", k*(k-1), n, sum, term);
+        //n++;
     }
 
     printf("cos(%f) = %f\n", x, sum);
-    printf("Number of iterations: %d", n);
+    //printf("Number of iterations: %d", n);
 
     return 0;
 }
