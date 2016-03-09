10ce3bed31dfb9b8c1e0d103965f3ec2a3a8efe7
markarevz <mark_anthony_arevalo@dlsu.edu.ph>
2016-03-09 15:23:56 +0800

    Updated OPCODE

--- COMPARC Project #2/COMPARC Project 2/COMPARC Project 2/Opcode.cs
+++ COMPARC Project #2/COMPARC Project 2/COMPARC Project 2/Opcode.cs
@@ -4,15 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace COMPARC_Project_2
-{
+{   String DSUBU= "DSUBU";
+    String DDIVU= "DDIVU";
+    String DMODU= "DMODU";
+    String SLT= "SLT";
+    String SELEQZ = "SELEQZ";
+    /*
+    String BNEC
+    String LD
+    String SD
+    String DADDUI
+
+    String 
+     */
+    
     public class Opcode
     {
         private int opcodeType;     //  J-type, I-type, etc.
+        private String opcodeString;
+        private String instruction;
+        
+        public Opcode(String instruction)
+        {
+            this.instruction = instruction;
+        }
 
-        public Opcode()
+        public String getOpcode()
         {
 
         }
+
+        public char getRS()
+        {
+
+        }
+
+        public 
     }
 }
