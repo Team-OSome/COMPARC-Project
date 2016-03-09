ebe435566a7ce16f0625835264996afc66f03b38
hirokiasaba5 <hiroki_asaba@dlsu.edu.ph>
2016-03-09 15:29:59 +0800

    commit test

--- COMPARC Project #2/COMPARC Project 2/COMPARC Project 2/Instruction.cs
+++ COMPARC Project #2/COMPARC Project 2/COMPARC Project 2/Instruction.cs
@@ -12,17 +12,26 @@ namespace COMPARC_Project_2
         private Boolean valid;              // check if the instruction entered is valid
         private int instructionType;        //  branch instruction, load/store instruction, Arithmetic Instruction, etc.
         private Opcode opcode;
+        private String mnemonic;
         
         public Instruction(string instruction)
         {
             opcode = new Opcode();
             this.instruction = instruction;
-            this.checkValid();
+            this.setMnemonic();
+            //this.checkValid();
         }
 
         private void checkValid()
         {
-            
+            if(this.existingInstructions(this.instruction))
+            {
+                this.valid = true;
+            }
+            else
+            {
+                this.valid = false;
+            }
         }
 
         public Boolean getValid()
@@ -33,6 +42,40 @@ namespace COMPARC_Project_2
         public String getInstruction()
         {
             return this.instruction;
+        }
+
+        public void setMnemonic()
+        {
+            String[] mnemonic = instruction.Split();
+            Console.WriteLine(mnemonic[0]);
+            if (this.existingInstructions(mnemonic[0]))
+            {
+                Console.WriteLine("correct instruction");
+                this.mnemonic = mnemonic[0];
+            }
+            else
+            {
+                this.valid = false;
+            }
+
+        }
+
+        public Boolean existingInstructions(string instruction)
+        {
+            switch (instruction)
+            {
+                case "DSUBU":
+                case "DDIV":
+                case "DMODU":
+                case "SLT":
+                case "SELENEZ":
+                case "BNEC":
+                case "LD":
+                case "SD":
+                case "DADDIU":
+                case "BC": return true;
+                default: return false;
+            }
         }
     
     }
